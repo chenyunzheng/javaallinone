72cfd23442f7f69e76bd7f10a9e3efe9da48c27e
acodeur <ccyzhope@126.com>
2024-03-11 18:17:22 +0800

    feat: add i8n sample in spring

--- src/main/java/com/chris/allinone/AllinoneApplication.java
+++ src/main/java/com/chris/allinone/AllinoneApplication.java
@@ -1,16 +1,27 @@
 package com.chris.allinone;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
 
+import java.util.Locale;
+
+@Slf4j
 @SpringBootApplication(exclude = {RabbitAutoConfiguration.class, ElasticsearchDataAutoConfiguration.class})
 public class AllinoneApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AllinoneApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(AllinoneApplication.class, args);
+
+		//i8n
+		String message = context.getMessage("API_NOT_FOUND", new String[]{"/i8n", "hi"}, "接口未找到", new Locale("cn"));
+		System.out.println(message);
+		log.info(message);
+
+
 	}
 
 }
---
+++ src/main/java/com/chris/allinone/spring/skills/BeanConfig.java
@@ -0,0 +1,21 @@
+package com.chris.allinone.spring.skills;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+/**
+ * @author chrischen
+ */
+@Configuration
+public class BeanConfig {
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("message");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+}
