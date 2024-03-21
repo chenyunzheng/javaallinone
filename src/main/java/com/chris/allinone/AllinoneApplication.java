418e776d02a9458857aba8e2e6d0585780b59d9a
acodeur <ccyzhope@126.com>
2024-03-22 06:27:21 +0800

    feat: add example for spring context abilities, etc

--- src/main/java/com/chris/allinone/AllinoneApplication.java
+++ src/main/java/com/chris/allinone/AllinoneApplication.java
@@ -1,25 +1,54 @@
 package com.chris.allinone;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.Resource;
 
 import java.util.Locale;
 
+/**
+ * @author chrischen
+ */
 @Slf4j
 @SpringBootApplication(exclude = {RabbitAutoConfiguration.class, ElasticsearchDataAutoConfiguration.class})
 public class AllinoneApplication {
 
+	@SneakyThrows
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(AllinoneApplication.class, args);
 
-		//i8n
+		//i18n
 		String message = context.getMessage("API_NOT_FOUND", new String[]{"/i8n", "hi"}, "接口未找到", new Locale("cn"));
 		System.out.println(message);
 		log.info(message);
+
+		//资源加载
+		Resource fileResource = context.getResource("file://D:\\cyzhope\\code\\non-project\\allinone\\pom.xml");
+		System.out.println(fileResource.contentLength());
+		System.out.println(fileResource.getURL());
+		Resource httpResource = context.getResource("https://www.baidu.com");
+		System.out.println(httpResource.contentLength());
+		System.out.println(httpResource.getURL());
+		Resource classPathResource = context.getResource("classpath:k8s/demo.yml");
+		System.out.println(classPathResource.contentLength());
+		System.out.println(classPathResource.getURL());
+
+		//获取运行时环境
+		for (PropertySource<?> propertySource : context.getEnvironment().getPropertySources()) {
+			System.out.println(propertySource.getClass().getName());
+		}
+		System.out.println(context.getEnvironment().getProperty("spring.application.name"));
+		System.out.println(context.getEnvironment().getProperty("abcd"));
+		//事件发布
+
+
+		//类型转化
 
 
 	}
